8275169ff00ae2034709084fd61b1e672aa06586
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-10-26 15:51:13 -0500

    Refactored Table properties

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(25) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    release_date INT,
-    sales FLOAT,
+    name VARCHAR(50) NOT NULL DEFAULT 'untitled',
+    release_date INT UNSIGNED,
+    sales FLOAT UNSIGNED,
     genre VARCHAR(20),
     PRIMARY KEY (id)
 );
