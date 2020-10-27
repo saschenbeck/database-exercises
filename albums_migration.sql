37d4902814ce3311ed65843b5e52fafc24108aa3
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-10-27 10:52:17 -0500

    CRUD: Refactored albums_migration file to allow longer artist, name, and genre; inserted albums_seeder into the albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(25) NOT NULL,
-    name VARCHAR(50) NOT NULL DEFAULT 'untitled',
+    artist VARCHAR(35) NOT NULL,
+    name VARCHAR(75) NOT NULL DEFAULT 'untitled',
     release_date INT UNSIGNED,
     sales FLOAT UNSIGNED,
-    genre VARCHAR(20),
+    genre VARCHAR(35),
     PRIMARY KEY (id)
 );
