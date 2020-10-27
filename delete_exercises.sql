3afd0333d9011db5139cfe03ec1c29a8533f5b47
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-10-27 11:46:31 -0500

    Feat: created delete_exercises.sql

---
+++ delete_exercises.sql
@@ -0,0 +1,40 @@
+USE codeup_test_db;
+
+-- SELECT 'Albums released after 1991' AS 'INFO';
+-- SELECT name, release_date
+-- FROM albums
+-- WHERE release_date > 1991;
+--
+-- DELETE FROM albums
+-- WHERE release_date > 1991;
+
+-- SELECT 'Albums released after 1991' AS 'INFO';
+-- SELECT name, release_date
+-- FROM albums
+-- WHERE release_date > 1991;
+
+-- SELECT 'Albums with disco genre' AS 'Info';
+-- SELECT name
+-- FROM albums
+-- WHERE genre = 'Disco';
+--
+-- DELETE FROM albums
+-- WHERE genre = 'Disco';
+--
+-- SELECT 'Albums with disco genre' AS 'Info';
+-- SELECT name
+-- FROM albums
+-- WHERE genre = 'Disco';
+
+-- SELECT 'Albums by Madonna' AS 'Info';
+-- SELECT name
+-- FROM albums
+-- WHERE artist = 'Madonna';
+--
+-- DELETE FROM albums
+-- WHERE artist = 'Madonna';
+--
+-- SELECT 'Albums by Madonna' AS 'Info';
+-- SELECT name
+-- FROM albums
+-- WHERE artist = 'Madonna';
