08757b69c06216b2055ca22c1d3d30aacdc6eb71
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-10-29 14:37:44 -0500

    Feat: found currrent salary of current department managers

--- join_exercises.sql
+++ join_exercises.sql
@@ -20,4 +20,14 @@ WHERE d_Manager.to_date = '9999-01-01' AND e.gender = 'F'
 ORDER BY Department_Name;
 
 # Find the current salary of all current managers.
+SELECT d.dept_name AS Department_Name, CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager, s.salary AS Salary
+FROM employees as e
+         JOIN dept_manager as d_Manager
+              ON d_Manager.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = d_Manager.dept_no
+         JOIN salaries as s
+              ON s.emp_no = e.emp_no
+WHERE d_Manager.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
+ORDER BY Department_Name;
 
